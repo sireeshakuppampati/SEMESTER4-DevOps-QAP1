799131d850419797310282ab18858d9445895884
sireesha <sireesha.kuppampati@keyin.com>
2024-10-15 18:15:49 -0230

    Set up GitHub Actions

--- src/main/java/com/bank/management/Main.java
+++ src/main/java/com/bank/management/Main.java
@@ -2,6 +2,14 @@ package com.bank.management;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bank bank = new Bank();
+        bank.createAccount("John Doe", 1000.0);
+
+        Account account = bank.getAccount("John Doe");
+        account.deposit(500);
+        System.out.println("Balance after deposit: " + account.getBalance());
+
+        account.withdraw(300);
+        System.out.println("Balance after withdrawal: " + account.getBalance());
     }
 }
